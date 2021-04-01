b034f0b80189969889b9f8144bd4191517a06b26
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-04-01 16:37:22 -0700

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,5 @@
-alert('Hello World');
-
-
-// This will be written as: SerinBob
-let myName = 'Serin';
-document.write(myName);
-myName = 'Bob';
-document.write(myName);
-
-/* This will be written as: SerinBobBob */
-document.write(myName);
-
-// This will be written as: SerinBobBobSmith
-let herName = 'John';
-herName = 'Smith';
-document.write(herName);
-
-let simpleAddition = 2+2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-// let 'size' doesn't work like that
-let newSize = 100 * 8;
-document.write(newSize);
-
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-let favoriteFood = 'Uni Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Raichu', height: 3, types: ['static', 'electric'] },
+  { name: 'Ditto', height: 1, types: ['transform', 'normal'] },
+  { name: 'Smoochum', height: 2, types: ['clairvoyant', 'ice'] }
+];
